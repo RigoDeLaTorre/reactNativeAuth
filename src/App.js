77337e471b4e45e183b235de6447bd3da4491411
Rigo De La Torre <rigoddd@gmail.com>
2019-02-01 21:50:38 -0800

    firebase auth spinnerloading iconimplemented

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { View, Text } from "react-native";
 import firebase from "firebase";
 // import firebase from "@firebase/app";
 // import "@firebase/auth";
-import { Header } from "./components/common";
+import { Header, Button, Spinner } from "./components/common";
 import LoginForm from "./components/LoginForm";
 
 class App extends Component {
+  state = { loggedIn: null };
   componentWillMount() {
     firebase.initializeApp({
       apiKey: "AIzaSyDIQId2kGhyGZJ6HJt8EbqTh4gNX6a9bW4",
@@ -16,12 +17,33 @@ class App extends Component {
       storageBucket: "authentication-8bb0c.appspot.com",
       messagingSenderId: "71756351351"
     });
+    // if user will have an object if they are signed inspect.
+    // if user signs out, this will return undefined or null
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return <Button>Log Out</Button>;
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
